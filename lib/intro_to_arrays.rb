137184116de6854fcaeb9d82115e379dada25966
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-18 14:08:19 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -15,7 +15,7 @@ def last_element(my_last_element)
 end
 def first_element_with_array_methods(first_country )
   first_country.first
-end 
+end
 def last_element_with_array_methods(last_country )
   last_country.last
-end 
+end
