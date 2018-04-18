b6888d3e30e1aa48a6ad500e94bdab423ec76d00
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-18 14:01:32 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -4,6 +4,6 @@ end
 def array_with_two_elements
   @my_two_array = [1, 2]
 end
-def first_element (@taylor_swift)
-    @taylor_swift.first
+def first_element(my_first_element)
+    my_first_element.first
 end
